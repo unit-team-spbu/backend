febfa40a01cd5201178d8589158651834394ca9f
MaxKuznets0v <max.kuznetsov@yahoo.com>
2020-10-12 11:29:43 +0300

    issues/9 files remove

--- config.py
+++
@@ -1,7 +0,0 @@
-from datetime import time
-
-TIME = time(3, 0, 0) # Time for daily update
-TIMER = 86400  # Update time in seconds
-CRAWLERS = ['it_events_crawler',
-            'crawler_name2',
-            'crawler_name3']
