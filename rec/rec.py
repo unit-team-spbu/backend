d7f3bea07db0358d66102a60eadb2591f82fc165
MaxKuznets0v <max.kuznetsov@yahoo.com>
2020-10-13 19:10:12 +0300

    issues/9 add time delay for update(), now we have to put new crawlers mannualy

--- rec/rec.py
+++ rec/rec.py
@@ -3,38 +3,70 @@ from nameko.rpc import RpcProxy
 from nameko.timer import timer
 from nameko.web.handlers import http
 import config as cfg
+from datetime import timedelta, datetime
+from time import sleep
 
 
 class REC:
     """Microservice for requesting events from every crawler"""
-    name = "raw_events_collector"
-    crawlers_rpc = list()
+    # Vars
     
-    def update_crawlers(self):
-        """Updating crawlers list"""
+    name = "raw_events_collector"
+    # Crawlers-----------------------------------
+    it_events_rpc = RpcProxy('it_events_crawler')
+    #--------------------------------------------
+
+    # Logic
+
+    def _update_crawlers(self):
+        """Updating crawlers list
+            (Is not used now due to inability to run crawlers dynamically)"""
         self.crawlers_rpc = list()
         for crawler in cfg.CRAWLERS:
             self.crawlers_rpc.append(RpcProxy(crawler))
 
-    @timer(interval=cfg.TIMER)
+    #API
+
+    @rpc
     def update(self):
         """Requests crawlers for events
+        :param now: flag for instant service launch (we don't wait for cfg.TIME)
         :returns a batch with all events"""
-        self.update_crawlers()
-        events = list()
         get_res = list()
+        events = list()
 
-        # requesting events from each crawler
-        for ev_rpc in self.crawlers_rpc:
-            get_res.append(ev_rpc.get_upcoming_events.call_async())
+        # requesting events from each crawler------------------------------
+        get_res.append(self.it_events_rpc.get_upcoming_events.call_async())
 
+        # Needs to be changed with every new crawler-----------------------
+        
+        print("Fetching data...")
         # getting results
+        count = 1
         for res in get_res:
             events += res.result()
+            print(f"[{count}/{len(get_res)}] crawlers ready")
+            count += 1
 
         # TODO: Pass events to the next service 
         return events
 
     @http('GET', '/events')
-    def force_update(self):
+    def force_update_handler(self, request):
+        return self.update()
+
+    @timer(interval=cfg.TIMER, eager=True)
+    def handle_update(self):
+        """Waiting for update time and calling update"""
+        now = datetime.now().time()
+        if now < cfg.TIME:
+            delta = timedelta(hours=(cfg.TIME.hour - now.hour),
+                        minutes=(cfg.TIME.minute - now.minute),
+                        seconds=(cfg.TIME.second - now.second))
+        elif now > cfg.TIME:
+            delta = timedelta(hours=(24 - now.hour + cfg.TIME.hour),
+                        minutes=(-now.minute + cfg.TIME.minute),
+                        seconds=(-now.second + cfg.TIME.second))
+        print(f"Waiting {delta} ...")
+        sleep(delta.total_seconds())
         return self.update()
