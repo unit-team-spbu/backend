7fe12928ba423543a62dd6e23cfcfb40c7da9360
MaxKuznets0v <max.kuznetsov@yahoo.com>
2020-10-13 19:08:12 +0300

    issues/9 fix dockerfile, add it_events_crawler in config

--- rec/config.py
+++ rec/config.py
@@ -2,6 +2,4 @@ from datetime import time
 
 TIME = time(3, 0, 0) # Time for daily update
 TIMER = 86400  # Update time in seconds
-CRAWLERS = ['it_events_crawler',
-            'crawler_name2',
-            'crawler_name3']
+CRAWLERS = ['it_events_crawler']  # Can't get services dynamically
