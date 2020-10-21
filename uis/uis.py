ef07d26d567cc763ad6a8885cbe72ab1ee788c55
Egor Gorban <39485676+egorgorban@users.noreply.github.com>
2020-10-21 12:49:28 +0300

    issues/29 upd-uis.py add-http-post fix-typos

--- uis/uis.py
+++ uis/uis.py
@@ -2,25 +2,30 @@ from nameko.web.handlers import http
 from nameko.rpc import rpc, RpcProxy
 from nameko.events import event_handler
 from nameko_mongodb import MongoDatabase
+from werkzeug.wrappers import Request, Response
 import json
-from bson.objectid import ObjectId
 
 
 class UIS:
     # Vars
-
+    
+    # users' interests service
     name = "uis"
-
+    
     db = MongoDatabase()
+    
+    # here will be the user-top-service
     next_service_rpc = RpcProxy('next_service')
+    
+    event_das_rpc = RpcProxy('event_das')
 
     # Logic
 
     def _add_questonnaire_data(self, new_q):
         '''
         had received message from API about new questionnaire
-         and therefore the dict
-         [user_id, [tag_1, tag_2, ..., tag_n]],
+         and therefore the list
+         [user_id, ['tag_1', 'tag_2', ..., 'tag_n']],
          should write the line into the db
          with columns:
              'user_id'
@@ -32,23 +37,17 @@ class UIS:
         '''
 
         user_id = new_q[0]
-        raw_tags_dict = new_q[1]
-        for raw_tag in raw_tags_dict:
+        raw_tags_list = new_q[1]
+        for raw_tag in raw_tags_list:
             raw_tag = {raw_tag: 1.0}
 
-        # каким-то образом получить базу данных
-        # collection = self.db["interests"]
-        # или
-        # collection = self.db.collection
-        # ничего из этого не работает, потому что у MongoDatabase нет таких методов (?)
-
         collection = self.db["interests"]
-        collection.insertOne(
-            {"_id": user_id, "raw_tags": raw_tags_dict, "count_changes": 1}
+        collection.insert_one(
+            {"_id": user_id, "raw_tags": raw_tags_list, "count_changes": 1}
         )
-        return {user_id: raw_tags_dict}
+        return {user_id: raw_tags_list}
 
-    def _update_like_data(self, message):
+    def _update_like_data(self, message, event_tags=[]):
         '''
         having had the like-event:
             [user_id, event_id]
@@ -62,20 +61,19 @@ class UIS:
         total_weight = (total_weight*count + \
                         new_weight_of_this_tag)/(count+1.0)
         ++count
+
+        event_tags - list of tags related to liked event
         '''
         collection = self.db["interests"]
         user_id = message[0]
-        user_tags = collection.findOne(
-            {"_id": ObjectId(user_id)},
+        user_tags = collection.find_one(
+            {"_id": user_id},
             {"_id": 0, "raw_tags": 1}
         )
-        count = user_tags = collection.findOne(
-            {"_id": ObjectId(user_id)},
+        count = collection.find_one(
+            {"_id": user_id},
             {"_id": 0, "count_changes": 1}
         )
-        event_id = message[1]
-        # get list of tags for event
-        event_tags = []
 
         for event_tag in event_tags:
             if event_tag in user_tags:
@@ -85,10 +83,9 @@ class UIS:
             total_weight = (total_weight*count + 1.0)/(count+1.0)
             user_tags[event_tag] = total_weight
 
-        collection.updateOne({
-            "count_changes": count+1,
-            "raw_tags": user_tags
-        })
+        collection.update_one(
+            {'_id': user_id}, {'$set': {"count_changes": count+1, "raw_tags": user_tags}})
+
         return {user_id: user_tags}
 
     @rpc
@@ -97,7 +94,7 @@ class UIS:
             questionnaire - [user_id, [tag_1, tag_2, ..., tag_n]]
         '''
         data = self._add_questonnaire_data(questionnaire)
-        self.next_service_rpc.update_preferences(data)
+        # self.next_service_rpc.update_preferences(data)
         return data
 
     @event_handler("like_service", "like")
@@ -105,6 +102,37 @@ class UIS:
         '''
         message - [user_id, event_id]
         '''
+        # нужно получить список тегов для мероприятия
+        # events = self.event_das_rpc.get_events_for_uis(message[1])
         data = self._update_like_data(message)
-        self.next_service_rpc.update_preferences(data)
+        # self.next_service_rpc.update_preferences(data)
         return data
+
+    @http("POST", "/newq")
+    def create_new_q_handler(self, request: Request):
+        content = request.get_data(as_text=True)
+        questionnaire = json.loads(content)
+        data = self._add_questonnaire_data(questionnaire)
+        # self.next_service_rpc.update_preferences(data)
+        return Response(status=201)
+
+        # POST http://localhost:8000/newq HTTP/1.1
+        # Content-Type: application/json
+        #
+        # [1324, [
+        #     'free', 'docker', 'forum', 'online'
+        # ]]
+
+    @http("POST", "/got_like")
+    def update_q_handler(self, request: Request):
+        content = request.get_data(as_text=True)
+        like_message = json.loads(content)
+        # events = self.event_das_rpc.get_events_for_uis(like_message[1])
+        data = self._update_like_data(like_message)
+        # self.next_service_rpc.update_preferences(data)
+        return Response(status=201)
+
+        # POST http://localhost:8000/got_like HTTP/1.1
+        # Content-Type: application/json
+        #
+        # [1324, 8325453]
