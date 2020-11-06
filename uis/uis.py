5ba320514940212373ecd6bd5ab464bfc9bb590f
Egor Gorban <39485676+egorgorban@users.noreply.github.com>
2020-11-06 16:29:13 +0300

    issues/58 change uis add info for likes, favs & cancel-reaction

--- uis/uis.py
+++ uis/uis.py
@@ -80,7 +80,9 @@ class UIS:
                 tags[new_tag] = weight
 
             collection.update_one(
-                {'_id': user_id}, {'$set': {"tags": tags, "q_tags": tags_list}})
+                {'_id': user_id},
+                {'$set': {"tags": tags, "q_tags": tags_list}}
+            )
             return [user_id, tags]
         else:
             collection.insert_one(
@@ -89,19 +91,25 @@ class UIS:
             )
             return [user_id, new_tags]
 
-    def _update_like_data(self, message, event_tags=[]):
+    def _update_reaction_data(self, message, event_tags=[], w=1.0, cancel=False):
         '''
-        having had the like-event:
+        having had the like-event or fav-event and so on:
             [user_id, event_id]
         changes weights due to the following principal:
             the weight of the tag equals to probability of the fact that
             that tag is presented in a random chosen event that the user has liked
         for that:
         assume that all tags are of the same authority
-        total_weight = (total_weight*count + \
-                        new_weight_of_this_tag)/(count+1.0)
-        ++count
+        total_weight = (total_weight*count +|- \
+                        new_weight_of_this_tag)/(count +|- w)
+        ++|--count
         event_tags - list of tags related to liked event
+
+        where `w` is weight of the reaction
+        like : w = 1.0
+        add to favs: w = 5.0
+
+        and +|- depends on adding or cancelling reaction
         '''
         collection = self.db["interests"]
         user_id = message[0]
@@ -113,21 +121,47 @@ class UIS:
             {"_id": user_id},
             {"_id": 0, "count_changes": 1}
         )['count_changes']
+        if cancel:
+            '''
+            getting reaction back
+            '''
+            for event_tag in event_tags:
+                try:
+                    total_weight = user_tags[event_tag]
+                    total_weight = (total_weight*count - w)/(count-w)
+                    user_tags[event_tag] = total_weight
+                except Exception:
+                    print(Exception, ' - !smth weird!')
+                    user_tags[event_tag] = 0.0
 
-        for event_tag in event_tags:
-            if event_tag in user_tags:
-                total_weight = user_tags[event_tag]
-            else:
-                total_weight = 0.0
-            total_weight = (total_weight*count + 1.0)/(count+1.0)
-            user_tags[event_tag] = total_weight
+            for user_tag in user_tags:
+                if user_tag not in event_tags:
+                    user_tags[user_tag] = user_tags[user_tag]*count/(count-w)
 
-        for user_tag in user_tags:
-            if user_tag not in event_tags:
-                user_tags[user_tag] = user_tags[user_tag]*count/(count+1.0)
+            collection.update_one(
+                {'_id': user_id},
+                {'$set': {"count_changes": count-w, "tags": user_tags}}
+            )
+        else:
+            '''
+            adding reaction
+            '''
+            for event_tag in event_tags:
+                if event_tag in user_tags:
+                    total_weight = user_tags[event_tag]
+                else:
+                    total_weight = 0.0
+                total_weight = (total_weight*count + w)/(count+w)
+                user_tags[event_tag] = total_weight
 
-        collection.update_one(
-            {'_id': user_id}, {'$set': {"count_changes": count+1, "tags": user_tags}})
+            for user_tag in user_tags:
+                if user_tag not in event_tags:
+                    user_tags[user_tag] = user_tags[user_tag]*count/(count+w)
+
+            collection.update_one(
+                {'_id': user_id},
+                {'$set': {"count_changes": count+w, "tags": user_tags}}
+            )
 
         return [user_id, user_tags]
 
@@ -150,14 +184,53 @@ class UIS:
         self.dispatch("make_top", data)
 
     @rpc
-    @event_handler("like_service", "like")
-    def update_q(self, message):
+    @event_handler("likes", "like")
+    def add_like(self, message):
         '''
         message - [user_id, event_id]
+        добавляем лайк
         '''
         event_tags = self.event_das_rpc.get_tags_by_id(message[1])
 
-        data = self._update_like_data(message, event_tags)
+        data = self._update_reaction_data(message, event_tags, 1.0)
+        self.dispatch("make_top", data)
+
+    @rpc
+    @event_handler("likes", "like_cancel")
+    def cancel_like(self, message):
+        '''
+        message - [user_id, event_id]
+        убираем лайк
+        '''
+        event_tags = self.event_das_rpc.get_tags_by_id(message[1])
+
+        data = self._update_reaction_data(message, event_tags, 1.0, True)
+        # True means we get reaction back
+        self.dispatch("make_top", data)
+
+    @rpc
+    @event_handler("favorites", "fav")
+    def add_fav(self, message):
+        '''
+        message - [user_id, event_id]
+        добавили в избранное
+        '''
+        event_tags = self.event_das_rpc.get_tags_by_id(message[1])
+
+        data = self._update_reaction_data(message, event_tags, 1.0)
+        self.dispatch("make_top", data)
+
+    @rpc
+    @event_handler("favorites", "fav_cancel")
+    def cancel_fav(self, message):
+        '''
+        message - [user_id, event_id]
+        убрали из избранного
+        '''
+        event_tags = self.event_das_rpc.get_tags_by_id(message[1])
+
+        data = self._update_reaction_data(message, event_tags, 1.0, True)
+        # True means we get reaction back
         self.dispatch("make_top", data)
 
     @rpc
@@ -165,7 +238,7 @@ class UIS:
         return self._get_weights_by_id(id)
 
     @http("POST", "/newq")
-    def create_new_q_handler(self, request: Request):
+    def create_new_q_http(self, request: Request):
         content = request.get_data(as_text=True)
         questionnaire = json.loads(content)
         data = self._add_questonnaire_data(questionnaire)
@@ -180,18 +253,17 @@ class UIS:
         # ]]
 
     @http("POST", "/got_like")
-    def update_q_handler(self, request: Request):
+    def add_like_http(self, request: Request):
         content = request.get_data(as_text=True)
         like_message = json.loads(content)
 
-        # нужно получить список тегов для мероприятия
         event = self.event_das_rpc.get_event_by_id(like_message[1])
 
         event_tags = event['tags']
         # this field does not exist yet
         # must be like ['tag_1', 'tag_2', ... , 'tag_n]
 
-        data = self._update_like_data(like_message, event_tags)
+        data = self._update_reaction_data(like_message, event_tags, 1.0)
         self.dispatch("make_top", data)
 
         return Response(status=201)
@@ -201,7 +273,55 @@ class UIS:
         #
         # [1324, 8325453]
 
+    @http("POST", "/cancel_like")
+    def cancel_like_http(self, request: Request):
+        content = request.get_data(as_text=True)
+        like_message = json.loads(content)
+
+        event = self.event_das_rpc.get_event_by_id(like_message[1])
+
+        event_tags = event['tags']
+        # this field does not exist yet
+        # must be like ['tag_1', 'tag_2', ... , 'tag_n]
+
+        data = self._update_reaction_data(like_message, event_tags, 1.0, True)
+        self.dispatch("make_top", data)
+
+        return Response(status=201)
+
+    @http("POST", "/got_fav")
+    def add_fav_http(self, request: Request):
+        content = request.get_data(as_text=True)
+        like_message = json.loads(content)
+
+        event = self.event_das_rpc.get_event_by_id(like_message[1])
+
+        event_tags = event['tags']
+        # this field does not exist yet
+        # must be like ['tag_1', 'tag_2', ... , 'tag_n]
+
+        data = self._update_reaction_data(like_message, event_tags, 5.0)
+        self.dispatch("make_top", data)
+
+        return Response(status=201)
+
+    @http("POST", "/cancel_fav")
+    def cancel_fav_http(self, request: Request):
+        content = request.get_data(as_text=True)
+        like_message = json.loads(content)
+
+        event = self.event_das_rpc.get_event_by_id(like_message[1])
+
+        event_tags = event['tags']
+        # this field does not exist yet
+        # must be like ['tag_1', 'tag_2', ... , 'tag_n]
+
+        data = self._update_reaction_data(like_message, event_tags, 5.0, True)
+        self.dispatch("make_top", data)
+
+        return Response(status=201)
+
     @http("GET", "/get_weights/<id>")
-    def get_weights_by_id_handler(self, request: Request, id):
+    def get_weights_by_id_http(self, request: Request, id):
         user_weights = self._get_weights_by_id(id)
         return json.dumps(user_weights, ensure_ascii=False)
